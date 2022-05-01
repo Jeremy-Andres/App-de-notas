6c8f4ba0a7d65903bfc481232ba7f2b2d2661b07
Jeremy-Andres <jaherreraquilodran@gmail.com>
2022-05-01 00:24:14 -0400

    reordenamiento de botones, adición de ventana para abrir archivo, funciona pero, no muestra el contenido del archivo seleccionado

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Menu, ttk
 from tkinter import filedialog as fd
+from tkinter import scrolledtext as st
 
 from constantes import *
 
@@ -16,11 +17,11 @@ class Ventana(ttk.Frame):
 
         self.marco_izq = tk.Frame()
         self.marco_izq.config(background="#FDFEFE")
-        self.marco_izq.place(relx=0.0, rely=0.0, relwidth=0.75 ,relheight=1)
+        self.marco_izq.place(relx=0.0, rely=0.0, relwidth=0.85 ,relheight=1)
 
         self.marco_der = tk.Frame()
         self.marco_der.config(background="#F2F4F4")
-        self.marco_der.place(relx=0.76, rely=0.0, relwidth=0.25 ,relheight=1)
+        self.marco_der.place(relx=0.85, rely=0.0, relwidth=0.15 ,relheight=1)
 
 
         self.etiqueta_entrada_texto = tk.Label(self.marco_izq,text="Ingrese su texto:")     #etiqueta de la entrada de texto
@@ -29,25 +30,32 @@ class Ventana(ttk.Frame):
 
         self.entrada_texto = tk.Text(self.marco_izq, font=("Calibri 10"))        #entrada de texto
         self.entrada_texto.config(background="#FCF3CF")
-        self.entrada_texto.place(relx=0.01, rely=0.05, relwidth=0.99, relheight= 0.45)
+        self.entrada_texto.place(relx=0.01, rely=0.05, relwidth=0.98, relheight= 0.45, )
 
+
+        self.boton_abrir_archivo = ttk.Button(self.marco_der,text="Abrir", command= lambda: abrir_archivos(self))
+        self.boton_abrir_archivo.place(relx=0.1, rely=0.12, relwidth=0.8)
 
         self.boton_guardar = ttk.Button(self.marco_der,text="Guardar", command= lambda: guardar_archivo(self))
         self.boton_guardar.place(relx=0.1, rely=0.05, relwidth=0.8)
 
-        self.boton_mostrar_archivos = ttk.Button(self.marco_der,text="Mostrar archivos")
-        self.boton_mostrar_archivos.place(relx=0.1, rely=0.13, relwidth=0.8)
-
-        self.boton_abrir_archivo = ttk.Button(self.marco_der,text="Abrir")
-        self.boton_abrir_archivo.place(relx=0.1, rely=0.21, relwidth=0.8)
-
+        
         def guardar_archivo(self):
             nombre_archivo = fd.asksaveasfilename(initialdir= current_directory, title = "Guardar como", filetypes= (("cody files",".cody"),("todos los archivos","*.*")) )
             if nombre_archivo != '':
-                archivo = open(nombre_archivo, "w", encoding= "utf-8")
+                archivo = open(nombre_archivo,"w", encoding= "utf-8")
                 archivo.write(self.entrada_texto.get("1.0", "end-1c"))
                 archivo.close()
                 print("archivo guardado")
+
+        def abrir_archivos(self):
+            nombre_archivo = fd.askopenfilename(initialdir= current_directory, title= "Abrir nota", filetypes= (("cody files", ".cody"),("todos los arcivos","*.*")))
+            if nombre_archivo != '':
+                archivo = open(nombre_archivo, "a+", encoding= "utf-8")
+                contenido = archivo.read()
+                self.entrada_texto.delete(1.0,"end-1c")
+                self.entrada_texto.insert(1.0, contenido)
+
 
         """def obtener_texto():
             entrada = self.entrada_texto.get("1.0", "end-1c")
